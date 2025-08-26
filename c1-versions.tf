ad78d1d94ba2310fbc6ebb51ee9c23cf31bc88bc
Julio Barbosa <julio.almeida31@hotmail.com>
2025-08-26 14:50:25 -0300

    Updating c1-versions provider and ec2-instances.tf

---
+++ c1-versions.tf
@@ -0,0 +1,22 @@
+# Terraform Settings block
+terraform {
+    required_version = ">=1.0.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+# provider block
+provider "aws" {
+    profile = "default" # aws credentials profile configured on your local desktop terminal
+    region = "us-east-1"
+}
+
+# Resource block
+#resource "aws_instance" "ec2demo" {
+#    ami = "ami-02556f6726aa38019" # Amazon Linux 2023 AMI 2023.8.20250818.0 x86_64 HVM kernel-6.1
+#    instance_type = "t3.micro"
+#}
--- ec2-instances.tf
+++ ec2-instances.tf
@@ -1,21 +1,10 @@
-# Terraform Settings block
-terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+# Resource block
+resource "aws_instance" "ec2demo" {
+    ami = "ami-00ca32bbc84273381" # Amazon Linux 2023 AMI 2023.8.20250818.0 x86_64 HVM kernel-6.1
+    instance_type = "t3.micro"
+    user_data = file("${path.module}/app1-install.sh")
+    tags = {
+        "Name" = "EC2 Demo"
     }
 }
 
-# provider block
-provider "aws" {
-    profile = "default" # aws credentials profile configured on your local desktop terminal
-    region = "sa-east-1"
-}
-
-# Resource block
-resource "awi_instance" "ec2demo" {
-    ami = "ami-02556f6726aa38019" # Amazon Linux 2023 AMI 2023.8.20250818.0 x86_64 HVM kernel-6.1
-    instance_type = "t3.micro"
-}
